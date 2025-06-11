5c4c32767f50e0603ab0ea44d9e6b51c996deefe
faiez22222 <faiezmohd43@gmail.com>
2025-06-12 00:22:34 +0530

    Revert "removed unneccery"
    This reverts commit 23475b9e9b7d37cc37eac3223a7862887b8086a0.

--- leffa/diffusion_model/unet_block_gen.py
+++ leffa/diffusion_model/unet_block_gen.py
@@ -779,8 +779,6 @@ class UNetMidBlock2DCrossAttn(nn.Module):
             if cross_attention_kwargs is not None
             else 1.0
         )
-        print('hidden state' , hidden_states.shape)
-        print('encoder_hidden_states',encoder_hidden_states.shape)
         hidden_states = self.resnets[0](hidden_states, temb, scale=lora_scale)
         for attn, resnet in zip(self.attentions, self.resnets[1:]):
             if self.training and self.gradient_checkpointing:
@@ -1248,6 +1246,7 @@ class CrossAttnDownBlock2D(nn.Module):
                 hidden_states = hidden_states[0]
             else:
                 hidden_states = resnet(hidden_states, temb, scale=lora_scale)
+                print('resnet_hidden_states',hidden_states.shape)
                 hidden_states, this_reference_feature_idx = attn(
                     hidden_states,
                     encoder_hidden_states=encoder_hidden_states,
@@ -1259,6 +1258,7 @@ class CrossAttnDownBlock2D(nn.Module):
                     this_reference_feature_idx=this_reference_feature_idx,
                 )
                 hidden_states = hidden_states[0]
+                print('attention_hidden_states' , hidden_states.shape)
 
             # apply additional residuals to the output of the last pair of resnet and attention blocks
             if i == len(blocks) - 1 and additional_residuals is not None:
